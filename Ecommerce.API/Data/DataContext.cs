a62857b802f7ad0f04a8bd9a2863b3bae1765fdb
Dino3K <barragan5907+1@gmail.com>
2025-05-06 16:21:21 -0500

    Base de datos

---
+++ Ecommerce.API/Data/DataContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Ecommerce.Shared.Entities;
+
+namespace Ecommerce.API.Data
+{
+    public class DataContext :DbContext
+    {
+        public DataContext(DbContextOptions<DataContext> options) : base(options) { }
+
+        public DbSet<Country>
+
+    }
+}
---
+++ Ecommerce_Shared/Entities/Country.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+
+namespace Ecommerce.Shared.Entities
+{
+    public class Country
+    {
+
+        public int Id { get; set; }
+        [Display (Name = "Pais")]
+        [Required (ErrorMessage ="El campo {0} es obligatorio")]
+        [MaxLength(100)]
+
+        public string Name { get; set; }
+    }
+}
